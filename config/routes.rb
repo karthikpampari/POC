8b6811f3ec580c9577550a1cf34bb807743fc594
karthikpampari <karthik.pampari@acsicorp.com>
2022-12-13 14:48:43 +0530

    flash

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "items/veg", to: "items#veg", as: "item_veg"
   get "order_items/delete", to: "order_items#delete", as: "order_item_delete_all"
   get "orders/place_order/:total", to: "orders#place_order",as: "place_order"
   get "orders/all_orders", to: "orders#all_orders", as: "all_orders"
